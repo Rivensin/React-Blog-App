c3559e7007d8ec810b538715d89dd5dce398f002
unknown <rivensinn@gmail.com>
2025-03-26 22:06:22 +0700

    configure gh-pages deployement

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Blog-App/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js', // Ensure this path matches your project setup
-    base: '/React-Blog-App/',
   },
 })
